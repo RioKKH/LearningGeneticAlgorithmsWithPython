5b8dad5d94079ad6970424d4182d355646317c56
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2023-02-14 10:15:21 +0900

    new file:   demo_selection_propotional.py 	new file:   demo_selection_tournament.py 	new file:   example_selection_tournament.py 	new file:   individual.py 	new file:   selection_tournament.py

---
+++ ch3/example_selection_tournament.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+from selection_tournament import selection_tournament
+from individual import Individual
+
+POPULATION_SIZE = 5
+random.seed(4)
+
+population = Individual.create_random_population(POPULATION_SIZE)
+selected = selection_tournament(population, group_size=3)
+
+print(f"Population: {population}")
+print(f"Selected: {selected}")
